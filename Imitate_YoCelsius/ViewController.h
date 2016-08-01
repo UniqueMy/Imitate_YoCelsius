870a9db01d053b199742391bb0ebbef3f4922819
ShengHao <641776671@qq.com>
2016-08-01 14:28:14 +0800

    First submit

---
+++ Imitate_YoCelsius/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Imitate_YoCelsius
+//
+//  Created by mac on 16/8/1.
+//  Copyright © 2016年 Unique. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Imitate_YoCelsius/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Imitate_YoCelsius
+//
+//  Created by mac on 16/8/1.
+//  Copyright © 2016年 Unique. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
